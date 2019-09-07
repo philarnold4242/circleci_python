1cc501f17f2f17a6713fb2594a02b235c7533492
Phil Arnold <phil.arnold@roche.com>
2019-09-07 14:29:48 +0200

    Updated

--- circleci_python/tests/test_mymod.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-import circleci_python.mymod as mm
-
-class TestJoke(TestCase):
-    def test_is_string(self):
-        s = mm.joke()
-
-        self.assertEqual(s, "he")
